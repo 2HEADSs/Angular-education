347efaf3c282acc1b02958c3de0817b9383485fb
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-11-09 22:22:15 +0200

    add data and properties in artice.components.ts

--- 04. Components/components-lab/articles-app/src/app/article/article.component.ts
+++ 04. Components/components-lab/articles-app/src/app/article/article.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Article } from '../models/article.model';
 
 @Component({
   selector: 'app-article',
@@ -7,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleComponent implements OnInit {
 
-  constructor() { }
+  private symbols: number = 250;
+  @Input() article!: Article;
+  @Input() articleDesc!: string;
+  descToShow!: string;
+  articleDescLen!: number;
+  showReadMoreBtn: boolean = true;
+  imageIsShown: boolean = false;
+  imageButtonTtile: string = "Show Image";
+
+  constructor() {
+    this.articleDescLen = 0;
+    this.descToShow = ''
+  }
 
   ngOnInit(): void {
   }
