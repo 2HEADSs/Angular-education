61ee83416f001c17ef10ad92f3c478e1560a2e5c
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-11-09 11:02:25 +0200

    clear html bacis template

--- 04. Components/components-app-demo/src/app/app.component.ts
+++ 04. Components/components-app-demo/src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // template: '<h1>Hello World, {{title}}!</h1>'
   templateUrl: './app.component.html',
+  // styles: ['h1 {color: red}']
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
