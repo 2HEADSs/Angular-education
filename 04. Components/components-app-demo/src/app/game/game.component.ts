e90012589fa3e31c55c16f1485a39586fee61d8f
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-11-09 13:19:10 +0200

    add functionality button for changing color of span text

--- 04. Components/components-app-demo/src/app/game/game.component.ts
+++ 04. Components/components-app-demo/src/app/game/game.component.ts
@@ -2,7 +2,9 @@ import { Component } from "@angular/core";
 
 interface Game {
     title: string,
-    price: number
+    price: number,
+    img: string,
+    raiting: number
     //price? - can exist or not
 }
 
@@ -13,9 +15,29 @@ interface Game {
 })
 
 export class GameComponent {
-    games: Game[]=[
-        {title:'Minecraft', price: 10},
-        {title:'Candy Crush', price: 0},
-        {title:'Counter Strike', price: 120},
+    shouldPriceBeGreen: boolean;
+    // shouldShowAdditionalContent?: boolean = false;
+    games: Game[] = [
+        {
+            title: 'Minecraft',
+            price: 10,
+            img: 'https://image.api.playstation.com/vulcan/img/cfn/11307uYG0CXzRuA9aryByTHYrQLFz-HVQ3VVl7aAysxK15HMpqjkAIcC_R5vdfZt52hAXQNHoYhSuoSq_46_MT_tDBcLu49I.png',
+            raiting: 1
+        },
+        {
+            title: 'Candy Crush', price: 0,
+            img: 'https://play-lh.googleusercontent.com/gU9NKwpgLDYA6LIYK4dnkAkVyqNHUfTIqklEiNuO4oZ2OCpWQhQdqhnDh8Yb9B8SWIM',
+            raiting: 5
+        },
+        {
+            title: 'Counter Strike',
+            price: 120,
+            img: 'https://cdn-images.win.gg/resize/w/1000/format/webp/type/progressive/fit/cover/path/wp/uploads/2021/08/the-complete-list-of-nvidia-settings-for-high-csgo-fps-in-2021.jpg',
+            raiting: 10
+        },
     ]
+
+    handleExpandContentClick(): void {
+        this.shouldPriceBeGreen = this.shouldPriceBeGreen ? false : true
+    }
 }
