dcac6ed55ef37e9ce4d12595cb2cd6acbf7d0dfc
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-11-11 12:37:04 +0200

    add lifecyclehook demo

--- 05. DI, Intro to RxJS, Services/demo-idakiev/demoRxJS/src/app/test/test.component.ts
+++ 05. DI, Intro to RxJS, Services/demo-idakiev/demoRxJS/src/app/test/test.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  styleUrls: ['./test.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TestComponent implements OnInit {
+export class TestComponent implements OnInit, OnChanges {
 
-  constructor() { }
+
+  @Input() users!: { name: string }[];
+
+
+  constructor(private cdRef: ChangeDetectorRef) {
+    this.cdRef.detach()
+  }
+
+  ngOnChanges(): void {
+    if (this.users.length > 4) {
+      this.cdRef.detectChanges();
+    }
+  }
 
   ngOnInit(): void {
+    this.cdRef.detectChanges()
   }
 
 }
