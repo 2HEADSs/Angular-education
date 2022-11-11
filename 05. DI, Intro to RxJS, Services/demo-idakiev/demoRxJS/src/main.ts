08d0dafc803cd8251f3693079d0339d46f8f4659
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-11-11 11:57:04 +0200

    add monkey patching demo

--- 05. DI, Intro to RxJS, Services/demo-idakiev/demoRxJS/src/main.ts
+++ 05. DI, Intro to RxJS, Services/demo-idakiev/demoRxJS/src/main.ts
@@ -10,3 +10,12 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+  //monkey patching
+const _setInterval = window.setInterval;
+(window as any).setInterval = function (...args: any[]): number {
+  console.log('setInterval was called', args);
+  return _setInterval.apply(this, args as any) as unknown as number
+
+}
