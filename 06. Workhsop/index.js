fb7cfda08e60c3bab9bc211fccc2a01385646ab8
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-11-13 15:05:07 +0200

    initialize new demo project and set url adres for localhost

---
+++ 06. Workhsop/index.js
@@ -0,0 +1,29 @@
+global.__basedir = __dirname;
+require('dotenv').config()
+const dbConnector = require('./config/db');
+// const mongoose = require('mongoose');
+const apiRouter = require('./router');
+const cors = require('cors');
+// const config = require('./config/config');
+const { errorHandler } = require('./utils');
+
+dbConnector()
+  .then(() => {
+    const config = require('./config/config');
+
+    const app = require('express')();
+    require('./config/express')(app);
+
+    app.use(cors({
+      origin: config.origin,
+      credentials: true
+    }));
+
+    app.use('/api', apiRouter);
+
+    app.use(errorHandler);
+    console.log(config.port);
+
+    app.listen(config.port, console.log(`Listening on port ${config.port}!`));
+  })
+  .catch(console.error);
