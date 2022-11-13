f2193ec08e6f58a6af1eb31e8aaafd6be110bfdd
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-11-13 16:54:11 +0200

    add footer and header component

---
+++ 06. Workhsop/angular-app-workshop/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CoreModule } from './core/core.module'
+
+// import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CoreModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
