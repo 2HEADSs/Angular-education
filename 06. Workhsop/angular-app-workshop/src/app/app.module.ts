5b1bdf476f128efb7b10d499a83f816a24b40244
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-11-14 12:52:54 +0200

    Update app.module.ts

--- 06. Workhsop/angular-app-workshop/src/app/app.module.ts
+++ 06. Workhsop/angular-app-workshop/src/app/app.module.ts
@@ -4,14 +4,24 @@ import { CoreModule } from './core/core.module'
 
 // import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ThemeListComponent } from './theme-list/theme-list.component';
+import { RecentPostsComponent } from './recent-posts/recent-posts.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MainComponent } from './main/main.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThemeListComponent,
+    RecentPostsComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
     CoreModule,
+    HttpClientModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
