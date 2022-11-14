b8c17287e06c7e0544f99b68533288d8bb074144
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-11-14 22:42:46 +0200

    add user interface and add HttpClientModule

--- 07. Modules and Routing/models-and-routng/src/app/app.module.ts
+++ 07. Modules and Routing/models-and-routng/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
@@ -12,9 +14,10 @@ import { CoreModule } from './core/core.module';
   ],
   imports: [
     BrowserModule,
+    CoreModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    CoreModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
