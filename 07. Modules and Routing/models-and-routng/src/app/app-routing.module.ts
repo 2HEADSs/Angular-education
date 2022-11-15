f0e10aa3f40630507db48e2669391f5df2b95a63
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-11-15 15:26:58 +0200

    update rourting

--- 07. Modules and Routing/models-and-routng/src/app/app-routing.module.ts
+++ 07. Modules and Routing/models-and-routng/src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostListComponent } from './post/list/list.component';
+import { UserListComponent } from './user/list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'user-list'
+  },
+  {
+    path: 'user-list',
+    component: UserListComponent
+  },
+  {
+    path: 'post-list',
+    component: PostListComponent
+  }
+];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const AppRoutingModule = RouterModule.forRoot(routes);
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
+
