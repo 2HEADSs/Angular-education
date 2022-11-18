f45b98f75fa9f4f4f86cf44f2126d4df39313b94
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-11-18 20:36:35 +0200

    add http request

--- Angular custom demo/demo-app-2/src/app/app.component.ts
+++ Angular custom demo/demo-app-2/src/app/app.component.ts
@@ -1,21 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IUser } from './interfaces/user';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'demo-app-2';
-  users = [
-    {
-      name: 'ivan',
-      age: 23
-    },
-    {
-      name: 'ivan',
-      age: 23
-    }
-  ]
+export class AppComponent implements OnInit {
+  users!: IUser[] | undefined
+
+
+  constructor(public userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.loadUser().subscribe(console.log)
+    this.userService.loadUser().subscribe(users => this.users = users)
+  
+    
+  }
+
+
 }
 
---
+++ Angular custom demo/demo-app-2/src/app/user/user.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IUser } from '../interfaces/user';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent  {
+
+  // @Input() users: IUser = []
+}
