0b7b3b921d6576a668c95a397ffcf34d223cbf10
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-11-24 15:47:00 +0200

    update functionality

--- Workhsop/angular-app-workshop-november-course/src/app/api.service.ts
+++ Workhsop/angular-app-workshop-november-course/src/app/api.service.ts
@@ -17,6 +17,10 @@ export class ApiService {
   loadThemes(){
     return this.httpClient.get<ITheme[]>(`${apiUrl}/themes`)
   }
+
+  loadTheme(id:number){
+    return this.httpClient.get<ITheme>(`${apiUrl}/themes/${id}`)
+  }
   // https://localhost:3000/api/posts?limit=5
   loadPosts(limit?: number){
     return this.httpClient.get<IPost[]>(`${apiUrl}/posts${limit ? `?limit=${limit}`: ''}`);
--- Workhsop/angular-app-workshop-november-course/src/app/theme/theme-routing.module.ts
+++ Workhsop/angular-app-workshop-november-course/src/app/theme/theme-routing.module.ts
@@ -1,6 +1,7 @@
 import { RouterModule, Routes } from "@angular/router";
 import { MainComponent } from "./main/main.component";
 import { NewThemeComponent } from "./new-theme/new-theme.component";
+import { ThemeResolver } from "./resolvers/theme.resolver";
 import { ThemeDetailComponent } from "./theme-detail/theme-detail.component";
 import { ThemeListComponent } from "./theme-list/theme-list.component";
 
@@ -14,6 +15,9 @@ const routes: Routes = [
             },
             {
                 path: 'new',
+                resolve: {
+                    theme: ThemeResolver
+                },
                 component: NewThemeComponent
             },
             {
