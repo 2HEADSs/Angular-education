ab1c28abc8c7bfbca007ea94bae227284f4978ee
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-11-25 19:30:50 +0200

    update demo

--- 09.Forms/form-app/src/app/my-router-link.directive.ts
+++ 09.Forms/form-app/src/app/my-router-link.directive.ts
@@ -1,9 +1,11 @@
-import { Directive, ElementRef, OnDestroy, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Input, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appMyRouterLink]'
 })
-export class MyRouterLinkDirective implements OnDestroy {
+export class MyRouterLinkDirective implements OnInit, OnDestroy {
+
+  @Input() appMyRouterLink!: string
 
   unsub: (() => void) | undefined;
 
@@ -15,6 +17,11 @@ export class MyRouterLinkDirective implements OnDestroy {
     this.unsub = this.renderer.listen(this.elementRef.nativeElement, 'mouseleave', this.mouseLeaveHandler)
     // this.renderer.setAttribute(this.elementRef.nativeElement, 'data-test', '123')
 
+  }
+
+  ngOnInit(): void {
+    console.log(this.appMyRouterLink);
+    
   }
 
   mouseoverHandler = (e: MouseEvent) => {
