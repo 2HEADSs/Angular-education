572eb6cf35380f803c6ee892166e67d8a1d27fd5
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-11-25 14:52:24 +0200

    extended demo

--- 09.Forms/form-app/src/app/my-router-link.directive.ts
+++ 09.Forms/form-app/src/app/my-router-link.directive.ts
@@ -1,16 +1,31 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, OnDestroy, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appMyRouterLink]'
 })
-export class MyRouterLinkDirective {
+export class MyRouterLinkDirective implements OnDestroy {
+
+  unsub: (() => void) | undefined;
 
   constructor(
     private elementRef: ElementRef,
     private renderer: Renderer2
   ) {
-    this.renderer.setAttribute(this.elementRef.nativeElement, 'data-test', '123')
+    this.unsub = this.renderer.listen(this.elementRef.nativeElement, 'mouseover', this.mouseoverHandler)
+    this.unsub = this.renderer.listen(this.elementRef.nativeElement, 'mouseleave', this.mouseLeaveHandler)
+    // this.renderer.setAttribute(this.elementRef.nativeElement, 'data-test', '123')
 
+  }
+
+  mouseoverHandler = (e: MouseEvent) => {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'red')
+  }
+  mouseLeaveHandler = (e: MouseEvent) => {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'initial')
+  }
+
+  ngOnDestroy(): void {
+    this.unsub?.call(undefined)
   }
 
 }
