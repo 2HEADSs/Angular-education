888da1381e28f0b9ec92ab288c56650f0731b2c1
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-11-27 16:40:40 +0200

    update profile page

--- Workhsop/angular-app-workshop-november-course/src/app/auth/profile/profile.component.ts
+++ Workhsop/angular-app-workshop-november-course/src/app/auth/profile/profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { appEmailDomains } from 'src/app/shared/constants';
+import { appEmailValidator } from 'src/app/shared/validators';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  get user() {
+    const { username, email, tel: telephone } = this.authService.user!;
+    const [ext, ...tel] = telephone.split(' ')
+    return {
+      username,
+      email,
+      tel: tel.join(''),
+      ext
+    }
+  }
+
+  form = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    email: ['', [Validators.required, appEmailValidator(appEmailDomains)]],
+    ext: [''],
+    tel: [''],
+  })
+
+  constructor(private fb: FormBuilder, private authService: AuthService) { }
 
   ngOnInit(): void {
   }
