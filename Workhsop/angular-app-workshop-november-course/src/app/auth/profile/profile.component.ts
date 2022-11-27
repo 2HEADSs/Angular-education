4a54e8c94de384546d6c94ed0c25d13b899e7d02
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-11-27 23:14:53 +0200

    update profile page and edit functionality

--- Workhsop/angular-app-workshop-november-course/src/app/auth/profile/profile.component.ts
+++ Workhsop/angular-app-workshop-november-course/src/app/auth/profile/profile.component.ts
@@ -9,9 +9,10 @@ import { AuthService } from '../auth.service';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent{
+export class ProfileComponent {
 
   showEditMode = false;
+  formSubmited = false
 
   get user() {
     const { username, email, tel: telephone } = this.authService.user!;
@@ -31,18 +32,33 @@ export class ProfileComponent{
     tel: [''],
   })
 
-  constructor(private fb: FormBuilder, private authService: AuthService) { 
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+
     this.form.setValue(this.user)
   }
 
-  toggleEditMode():void{
+  toggleEditMode(): void {
+
     this.showEditMode = !this.showEditMode
+    if(this.showEditMode){
+      this.formSubmited = false
+    }
+
+
   }
 
-  saveProfile(): void{
-    if(this.form.invalid){return}
-    console.log(this.form.value);
-    
+  saveProfile(): void {
+    this.formSubmited = true
+    console.log(`This form is invalid ${this.form.invalid}`);
+      /// RETURN HERE
+    if (this.form.invalid) { return; }
+
+    const { username, email, ext, tel } = this.form.value
+    this.authService.user = {
+      username, email, tel: ext + ' ' + tel
+    } as any;
+    this.toggleEditMode();
+
   }
 
 }
