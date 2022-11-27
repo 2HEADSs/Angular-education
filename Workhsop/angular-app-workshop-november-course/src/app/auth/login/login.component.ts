6a72d14f4a7af554b2c5700f582350883331c911
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-11-27 12:06:59 +0200

    update login html component

--- Workhsop/angular-app-workshop-november-course/src/app/auth/login/login.component.ts
+++ Workhsop/angular-app-workshop-november-course/src/app/auth/login/login.component.ts
@@ -1,26 +1,27 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { AuthService } from '../auth.service';
+  import { NgForm} from '@angular/forms';
+  import { Component } from '@angular/core';
+  import { ActivatedRoute, Router } from '@angular/router';
+  import { AuthService } from '../auth.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
-})
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
+  })
 export class LoginComponent {
 
   constructor(private activatedRoute: ActivatedRoute, private router: Router, private authService: AuthService) {
 
   }
 
-  loginHandler(): void {
-    this.authService.user = {
-      username: 'John',
-      lastName: 'Doe'
-    } as any;
+  loginHandler(form: NgForm): void {
+    // this.authService.user = {
+    //   username: 'John',
+    //   lastName: 'Doe'
+    // } as any;
 
-    const returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'] || '/';
+    // const returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'] || '/';
 
-    this.router.navigate([returnUrl])
+    // this.router.navigate([returnUrl])
   }
 }
