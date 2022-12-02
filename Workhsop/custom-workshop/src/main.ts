8bdbbfa7d59b4cab39f4222a072a714f40c25b0b
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-12-02 14:14:17 +0200

    add subject-ibserve-observable demo

--- Workhsop/custom-workshop/src/main.ts
+++ Workhsop/custom-workshop/src/main.ts
@@ -1,12 +1,55 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { enableProdMode } from '@angular/core';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { Observable, Subject, take } from "rxjs";
 
-if (environment.production) {
-  enableProdMode();
-}
+// import { AppModule } from './app/app.module';
+// import { environment } from './environments/environment';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// if (environment.production) {
+//   enableProdMode();
+// }
+
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
+
+const subject = new Subject()
+
+let observable$ = new Observable(observer => {
+  let counter = 1;
+
+  const id = setInterval(() => {
+    observer.next(counter++)
+  }, 1000);
+
+  return () => {
+    clearInterval(id)
+  }
+});
+
+observable$.subscribe(subject)
+
+
+
+const firstSubscription = observable$.subscribe({
+  next: (value) => { console.log('firstSubscription' + value); },
+  complete: () => console.log('complete firstSubscription'),
+})
+
+
+const subjectSubscribe= subject.subscribe({
+  next: (value) => { console.log('subjectSubscribe' + value); },
+  complete: () => console.log('complete subjectSubscribe'),
+})
+
+
+setTimeout(() => {
+  firstSubscription.unsubscribe()
+  subjectSubscribe.unsubscribe()
+  const secondSubstription = observable$.pipe(take(3)).subscribe({
+    next: (value) => {
+      console.log('secondSubstription' + value);
+    },
+    complete: () => console.log('complete secondSubstription'),
+  });
+}, 2000)
