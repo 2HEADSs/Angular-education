71710f2d11b352c96848b8ef19100796b16c8290
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-12-02 20:05:20 +0200

    update demo - hot and cold observable

--- Workhsop/custom-workshop/src/main.ts
+++ Workhsop/custom-workshop/src/main.ts
@@ -13,10 +13,10 @@ import { Observable, Subject, take } from "rxjs";
 // platformBrowserDynamic().bootstrapModule(AppModule)
 //   .catch(err => console.error(err));
 
-const subject = new Subject()
+const subject$$ = new Subject()
 
 let observable$ = new Observable(observer => {
-  let counter = 1;
+  let counter = 0;
 
   const id = setInterval(() => {
     observer.next(counter++)
@@ -27,29 +27,39 @@ let observable$ = new Observable(observer => {
   }
 });
 
-observable$.subscribe(subject)
-
+observable$.subscribe(subject$$)
 
 
 const firstSubscription = observable$.subscribe({
-  next: (value) => { console.log('firstSubscription' + value); },
+  next: (value) => { console.log('firstSubscription ' + value); },
   complete: () => console.log('complete firstSubscription'),
 })
 
 
-const subjectSubscribe= subject.subscribe({
-  next: (value) => { console.log('subjectSubscribe' + value); },
-  complete: () => console.log('complete subjectSubscribe'),
+const subjectSubscribe= subject$$.subscribe({
+  next: (value) => { console.log('SUBject1.1 ' + value); },
+  complete: () => console.log('complete SUBject1.1'),
 })
 
 
 setTimeout(() => {
   firstSubscription.unsubscribe()
+
   subjectSubscribe.unsubscribe()
+   
   const secondSubstription = observable$.pipe(take(3)).subscribe({
     next: (value) => {
-      console.log('secondSubstription' + value);
+      console.log('secondSubstription ' + value);
     },
     complete: () => console.log('complete secondSubstription'),
   });
+
+   subject$$.pipe(take(3)).subscribe({
+    next: (value) => {
+      console.log('SUBject2.1 ' + value);
+    },
+    complete: () => console.log('complete SUBject2.1'),
+  });
+
+
 }, 2000)
