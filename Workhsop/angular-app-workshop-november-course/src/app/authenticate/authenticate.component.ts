5c77b62c4e5f7ddf53c66cf38584ab1918716194
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-12-03 23:25:22 +0200

    add authentication component

---
+++ Workhsop/angular-app-workshop-november-course/src/app/authenticate/authenticate.component.ts
@@ -0,0 +1,18 @@
+
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+
+@Component({
+  selector: 'app-authenticate',
+  templateUrl: './authenticate.component.html',
+  styleUrls: ['./authenticate.component.scss']
+})
+export class AuthenticateComponent implements OnInit {
+
+  constructor(private authService:AuthService) { }
+
+  ngOnInit(): void {
+    this.
+  }
+
+}
